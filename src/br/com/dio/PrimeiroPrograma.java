ad7b7deb3d692e2875d0dc2c181f0ba89fb3024d
jedualmeida <joseeduardoalmeida@live.com>
2023-03-11 20:18:35 -0300

    primeiro commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,25 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		gato gato = new gato();
+		livros livros = new livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello World!" + (a+b));*/
+		
+	}
+
+}
+
+class livros {
+	private String nome;
+	private Integer npag;
+}
